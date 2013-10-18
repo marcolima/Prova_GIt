21051d7024ce72c2653f7c8af8d76a2b3e9ca2e9
Marco Lima <marco.lima.sv@gmail.com>
2013-10-18 12:48:30 +0200

    Adding First file

---
+++ Hello.cpp
@@ -0,0 +1,9 @@
+#include <cstdlib>  // per EXIT_SUCCESS
+#include <iostream> // per std::cout e std::endl
+ 
+int main()
+{
+   std::cout << "Hello World!" << std::endl;
+ 
+   return EXIT_SUCCESS;
+}
